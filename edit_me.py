e15fb4b52c689760632dbae4f57547193b036d44
Billy Tropepe <btropepe@drury.edu>
2025-01-22 12:12:13 -0600

    First Commit

--- edit_me.py
+++ edit_me.py
@@ -3,11 +3,11 @@ def find_max(lst):
     """return the maximum element"""
 
     current_max = lst[0]
-
-    #
-    #  YOUR CODE GOES HERE
-    #
-
+#
+    for i in lst:
+        if i>current_max:
+            current_max=i
+#
     return current_max
 
 test_list = [2112*i % 2024 for i in range(10000)]
